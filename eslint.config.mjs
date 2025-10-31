f7af3a0b6a30e192ecebb37ff0f8b2d15a220830
Victoria Demidenko <demidenko@adsfin.pro>
2025-10-31 13:51:03 +0300

    start

---
+++ eslint.config.mjs
@@ -0,0 +1,29 @@
+import { defineConfig, globalIgnores } from "eslint/config";
+import nextVitals from "eslint-config-next/core-web-vitals";
+import nextTs from "eslint-config-next/typescript";
+import prettier from "eslint-plugin-prettier";
+import prettierConfig from "eslint-config-prettier";
+
+const eslintConfig = defineConfig([
+  ...nextVitals,
+  ...nextTs,
+
+  prettierConfig,
+  {
+    plugins: { prettier },
+    rules: {
+      "prettier/prettier": "warn",
+    },
+  },
+
+  // Override default ignores of eslint-config-next.
+  globalIgnores([
+    ".next/**",
+    "out/**",
+    "build/**",
+    "next-env.d.ts",
+  ]),
+]);
+
+export default eslintConfig;
+
